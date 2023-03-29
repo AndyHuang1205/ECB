750c9060d060b2688133eaf10875579afed4a1eb
Andy Huang <113555041+AndyHuang1205@users.noreply.github.com>
2023-03-28 23:41:37 -0400

    Update main.go

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 var codebook = [2][4][2]int{{{0b00, 0b01}, {0b01, 0b10}, {0b10, 0b11}, {0b11, 0b00}}, {{0b00, 0b10}, {0b01, 0b11}, {0b10, 0b00}, {0b11, 0b01}}}
 var message = [4]int{0b00, 0b01, 0b10, 0b11}
 var key int = 0
+var lookupValue int = 0
 
 func textToBinary(s string) (binString string) {
 	for _, c := range s {
@@ -52,24 +53,13 @@ func codebookLookup(key int) (ciphertext int) {
 	}
 	return ciphertext
 }
-
-func main() {
-	var text string
-
-	fmt.Printf("Enter your message: ")
-	fmt.Scanln(&text)
-	text = textToBinary(text)
-	fmt.Println("Binary plaintext: " + text)
-
+func ECB_Ciphertext(text string) (ciphertext string) {
+	ciphertext = ""
 	var x, i int = 0, 0
-	var lookupValue int = 0
-	var ciphertext string = ""
-	var plaintextBinary string = ""
 	for x = 0; x < len(text)-1; x += 2 {
 		binaryInt, _ := strconv.ParseInt(text[x:x+2], 2, 0)
 		t := strconv.FormatInt(binaryInt, 10)
 		for i = 0; i < len(codebook[key]); i++ {
-			//println(strconv.Itoa(codebook[key][i][1]))
 			if t == strconv.Itoa(codebook[key][i][1]) {
 				lookupValue = codebook[key][i][0]
 				fmt.Printf("The ciphered value is %b\n", lookupValue)
@@ -80,7 +70,11 @@ func main() {
 		}
 	}
 	println("Ciphertext: " + ciphertext)
+	return ciphertext
+}
 
+func ECB_Decipher(ciphertext string) (plaintext string) {
+	var plaintextBinary string = ""
 	var k, l int = 0, 0
 	for k = 0; k < len(ciphertext)-1; k += 2 {
 		binaryInt, _ := strconv.ParseInt(ciphertext[k:k+2], 2, 0)
@@ -95,6 +89,17 @@ func main() {
 		}
 	}
 	println("deciphered plaintext: " + plaintextBinary)
-	plaintext := binaryToString(plaintextBinary)
+	plaintext = binaryToString(plaintextBinary)
 	println("deciphered text: " + plaintext)
+	return plaintext
+}
+
+func main() {
+	var text string
+	fmt.Printf("Enter your message: ")
+	fmt.Scanln(&text)
+	text = textToBinary(text)
+	fmt.Println("Binary plaintext: " + text)
+	ciphertext := ECB_Ciphertext(text)
+	ECB_Decipher(ciphertext)
 }
